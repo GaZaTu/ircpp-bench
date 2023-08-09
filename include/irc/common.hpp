1be33ce2157a5110f64fd8d41fc4722dafb43168
GaZaTu <gazatu@protonmail.com>
2023-08-09 22:09:27 +0200

    xd

--- include/irc/common.hpp
+++ include/irc/common.hpp
@@ -33,7 +33,7 @@ public:
   tags_map() {
   }
 
-  size_t size() {
+  size_t size() const {
     return _list.size();
   }
 
@@ -64,7 +64,12 @@ private:
 };
 
 std::string_view consumeTags(std::string_view raw, tags_map& tags) {
-  if (raw[0] != '@') {
+  constexpr char TAGS_BEGIN = '@';
+  constexpr char KEY_END = '=';
+  constexpr char VALUE_END = ';';
+  constexpr char TAGS_END = ' ';
+
+  if (raw[0] != TAGS_BEGIN) {
     return raw;
   }
 
@@ -76,7 +81,7 @@ std::string_view consumeTags(std::string_view raw, tags_map& tags) {
   namespace std_simd = std::experimental;
 #endif
 
-  std_simd::native_simd<decltype(raw)::value_type> vector;
+  std_simd::native_simd<char> vector;
 
   std::string_view key;
   std::string_view value;
@@ -85,24 +90,25 @@ std::string_view consumeTags(std::string_view raw, tags_map& tags) {
   for (size_t i = offset; i < raw.length(); i += vector.size()) {
     vector.copy_from(&raw[i], std_simd::element_aligned);
 
-    auto mask = (vector == '=') || (vector == ';') || (vector == ' ');
+    auto mask = (vector == KEY_END) || (vector == VALUE_END) || (vector == TAGS_END);
     while (std_simd::any_of(mask)) {
       auto end_idx_in_vector = std_simd::find_first_set(mask);
       mask[end_idx_in_vector] = false;
 
-      auto view = raw.substr(offset, (i + end_idx_in_vector) - offset);
-      offset = i + end_idx_in_vector + 1;
+      auto end_idx = i + end_idx_in_vector;
+      auto view = raw.substr(offset, end_idx - offset);
+      offset = end_idx + 1;
 
-      switch (raw[offset - 1]) {
-      case '=':
+      switch (raw[end_idx]) {
+      case KEY_END:
         key = view;
         break;
 
-      case ';':
+      case VALUE_END:
         tags.emplace(key, view);
         break;
 
-      case ' ':
+      case TAGS_END:
         tags.emplace(key, view);
         goto consume_tags_end;
       }
